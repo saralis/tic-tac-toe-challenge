7beab68e560d658f0f97026578d0cc82d8ce7081
sergener <sibel.ergener@gmail.com>
2016-05-17 10:02:01 -0400

    Add files and basic html and css

---
+++ tic-tac-toe.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {}
+
+//start:
+//x goes first
+//each player takes a turn?
+//logic to win tic-tac-toe?
+
+function Player(position) {
+	this.position = position;
+	this.
+}
